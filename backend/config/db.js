e8d3ab3192ae028de56ab35e796dad7f5b51b559
Abhijeet mallik <114431322+abhijeetmallik@users.noreply.github.com>
2024-11-19 21:53:10 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://FoodCourt:abhifoodcourt@cluster0.pjqk1.mongodb.net/LPU_Food_Court').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb').then(()=>console.log("DB Connected"));
 }
